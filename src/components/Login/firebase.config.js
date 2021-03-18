91863d1294d5b14a7e21a826e7c14be622b066e0
rahat3739 <snrahat.stud4143@gmail.com>
2021-03-18 17:16:03 +0600

    bookimg room are coming soon

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGb_ve9bOVQXCz-dDoTmto44VxQ43tFsE",
+    authDomain: "burj-al-arab-90cdf.firebaseapp.com",
+    projectId: "burj-al-arab-90cdf",
+    storageBucket: "burj-al-arab-90cdf.appspot.com",
+    messagingSenderId: "687847123970",
+    appId: "1:687847123970:web:5b43fada7fd907466b4b1b"
+  };
+
+  export default firebaseConfig;
